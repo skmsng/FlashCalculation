7fd2f9a0561faa7de0c667b9220863176eb36946
smnksg <smnksg-2012@yahoo.co.jp>
2012-08-06 23:53:30 +0900

    2012.8.6-2

--- src/com/example/application1/MainActivity.java
+++ src/com/example/application1/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.application1;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Message;
+//import android.os.Message;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
@@ -11,21 +11,31 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+/**
+ * 画面の定期更新をするには、スレッドのrunメソッド内で下記のどちらかのメソッドを使う
+ * １．Handlerクラスのpostメソッド
+ * ２．HandlerクラスのsendEmptyMessageメソッド（別途handleMessageメソッドを定義する）
+ */
+
 public class MainActivity extends Activity {
 	
 	int total;		//計算結果
-	int random;		//表示する乱数
-	int count;		//表示回数
-	int delaytime;	//表示間隔
-	TextView view = null;
-	SampleHandler handler = null;
+	int random;		//乱数
+	int count = 10;	//表示回数
+	long delaytime;	//表示間隔(ミリ秒)
+	boolean flag;	//画面表示、非表示
+	
+	TextView tv;
+	Handler handler;
+	
+//	SampleHandler handler;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        view = (TextView) this.findViewById(R.id.display2);
+        tv = (TextView) this.findViewById(R.id.display2);
     }
 
     @Override
@@ -34,7 +44,94 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    /**
+     * auto
+     * 自動
+     */
+    public void autoKeyOnClick(View v){		
+    	
+    	//表示間隔
+    	Button button = (Button)v;
+    	switch(button.getId()){								
+		case R.id.autobutton1:
+			delaytime = 100;
+			System.out.println(delaytime);
+			break;
+		case R.id.autobutton2:
+			delaytime = 1000;
+			System.out.println(delaytime);
+			break;
+		case R.id.autobutton3:
+			delaytime = 2000;
+			System.out.println(delaytime);
+			break;
+    	}
+    	
+    	handler = new Handler();
+
+    	LoopThread thread = new LoopThread();
+    	thread.start();
+    	
+    	
+    }
     
+    /**
+     * スレッド
+     * インナークラス
+     */
+    public class LoopThread extends Thread {
+    	public void run(){
+    		
+    		flag = true;
+    		
+    		for(int i=0; i<count*2; i++){
+
+        		try{
+    				Thread.sleep(delaytime/2);
+    			}catch(Exception e){
+    			}
+
+        		/**
+        		 * postメソッド
+        		 */
+        		handler.post(new Runnable(){
+        			public void run(){
+        				
+                		if(flag){
+            				RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radiogroup);
+            		    	RadioButton radioButton = (RadioButton) findViewById(radioGroup.getCheckedRadioButtonId());
+            		    	switch(radioButton.getId()){								
+            				case R.id.radiobutton1:
+            					System.out.println("低");
+            					random = (int)Math.floor(Math.random() * 9) + 1;
+            			    	total += random;
+            					break;
+            				case R.id.radiobutton2:
+            					System.out.println("中");
+            					random = (int)Math.floor(Math.random() * 10) + 10;
+            			    	total += random;
+            					break;
+            				case R.id.radiobutton3:
+            					System.out.println("高");
+            					random = (int)Math.floor(Math.random() * 8 + 2) * 10 + (int)Math.floor(Math.random() * 10);
+            			    	total += random;
+            					break;
+            		    	}
+            				tv.setText(Integer.toString(random));
+            				flag = false;
+            				
+            			}else{
+            				tv.setText("");
+            				flag = true;
+            			}
+        			}
+        		});
+        		
+    		}
+    	}
+    }
+    
+
     /**
      * start
      * 手動
@@ -60,7 +157,7 @@ public class MainActivity extends Activity {
 			break;
     	}
     	
-    	view.setText(Integer.toString(random));
+    	tv.setText(Integer.toString(random));
     	
     	System.out.println("計算結果:" + total);
     }
@@ -71,8 +168,7 @@ public class MainActivity extends Activity {
      */
     public void endKeyOnClick(View v){
 
-    	//TextView view = (TextView)this.findViewById(R.id.display2);
-    	view.setText(Integer.toString(total));
+    	tv.setText(Integer.toString(total));
     	
     	System.out.println("計算結果:" + total);
     	total = 0;
@@ -85,25 +181,15 @@ public class MainActivity extends Activity {
     	}
     }
     
-    /**
-     * reset
-     * リセット
-     /
-    public void resetKeyOnClick(View v){
-    	
-    	total = 0;
+    
 
-    	//TextView view = (TextView)this.findViewById(R.id.display2);
-    	view.setText(Integer.toString(total));
-    	
-    	System.out.println("計算結果:" + total);
-    	
-    }
     
     /**
      * auto
      * 自動
-     */
+     * 2012.8.5発表時に使っていた画面更新方法
+     * 上記2の方法だけどスレッドを使っていない
+     *
     public void autoKeyOnClick(View v){
     	Button button = (Button)v;
     	switch(button.getId()){								
@@ -127,10 +213,9 @@ public class MainActivity extends Activity {
     	handler.sleep(0);
     }
 
-    
 	/**
 	 * @see android.app.Activity#onResume()
-	 */
+	 *
 	@Override
 	protected void onResume() {
 		super.onResume();
@@ -139,21 +224,23 @@ public class MainActivity extends Activity {
 
 	/**
 	 * @see android.app.Activity#onStop()
-	 */
+	 *
 	@Override
 	protected void onStop() {
 		super.onStop();
 		handler = null;
 	}
 
-    
+    /**
+     * sleepメソッドを呼ぶことでhandleMessageメソッドが自動更新される
+     * 
 	public class SampleHandler extends Handler {
 		boolean flag = true;
 
 		@Override
 		public void handleMessage(Message msg) {
 			super.handleMessage(msg);
-			view.invalidate();
+			tv.invalidate();
 			if (handler != null) handler.sleep(delaytime);
 			
 			System.out.println(++count);
@@ -181,11 +268,11 @@ public class MainActivity extends Activity {
 			    	total += random;
 					break;
 		    	}
-				view.setText(Integer.toString(random));
+				tv.setText(Integer.toString(random));
 				flag = false;
 				
 			}else{
-				view.setText("");
+				tv.setText("");
 				flag = true;
 			}
 			
@@ -197,14 +284,5 @@ public class MainActivity extends Activity {
 		}
 
 	}
-    
+	*/
 }
-    
-    
-//try{
-//Thread.sleep(2000);
-//}catch(Exception e){
-//System.out.print(e);
-//}
-    
-// test
